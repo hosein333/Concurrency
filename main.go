7c1c7110ba18962b2a28d2aa8f71e11a037cb969
mohammadjavad <hossinbabayy1382@gmail.com>
2024-01-22 12:31:36 +0330

    eror

--- main.go
+++ main.go
@@ -5,27 +5,34 @@ import (
 	"sync"
 )
 
-func sayHello(wg *sync.WaitGroup) {
-	wg.Done()
+func sayHello(wg *sync.WaitGroup, pingChannel chan string, pongChannel chan string) {
         for i := 0; i < 5; i++ {
                 fmt.Println("Hello")
+		pingChannel <- "World"
+		<-pongChannel 
         }
-}
-func sayWorld(wg *sync.WaitGroup) {
 	wg.Done()
+}
+func sayWorld(wg *sync.WaitGroup, pingChannel chan string, pongChannel chan string) {
         for i := 0; i < 5; i++ {
-                fmt.Println("World")
+		v := <-pongChannel
+                fmt.Println(v)
+		pongChannel <- "pong"
         }
+	wg.Done()
 }
 func main() {
 	var wg sync.WaitGroup
+
+	pingChannel := make(chan string)
+	pongChannel := make(chan string)
 	
 	wg.Add(1)
-	go sayHello(&wg)
-	wg.Wait()
+	go sayHello(&wg, pingChannel, pongChannel)
 
 	wg.Add(1)
-        go sayWorld(&wg)
+        go sayWorld(&wg, pingChannel, pongChannel)
+
         wg.Wait()
 
         fmt.Println("Finished")
